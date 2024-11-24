81aa6c308a358a48f18f059da6122d2bc4baced6
JhonSoto99 <jofeso-bamba@hotmail.com>
2024-11-24 18:50:51 -0500

    feature(models): Create model tasks.

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    """Model to store tasks."""
+    title = models.CharField(max_length=128, help_text="Title of the task.")
+    email = models.EmailField("Email associated with the task.")
+    description = models.TextField(help_text="Description of the task.")
+    expiration_date = models.DateTimeField(help_text="Expiration date of the task.")
+
+    def __str__(self):
+        return f"{self.title}"

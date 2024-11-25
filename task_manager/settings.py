81ce397890fdf7d0358901e831ce8107e1c6ec16
JhonSoto99 <jofeso-bamba@hotmail.com>
2024-11-25 13:14:20 -0500

    feat(unit-test): Add unit test.

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "crispy_forms",
     "crispy_tailwind",
+    "django_extensions",
     "rest_framework",
     "tasks",
     "users",
--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from datetime import datetime
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from tasks.models import Task
+
+
+class TaskViewSetTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser", password="testpassword"
+        )
+
+        self.task = Task.objects.create(
+            user=self.user,
+            title="Example title",
+            description="Example description",
+            expiration_date=datetime.now(),
+        )
+        self.client = APIClient()
+
+    def test_list_should_return_200(self):
+        url = reverse("task-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)

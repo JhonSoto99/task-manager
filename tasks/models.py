3c0c5a7c36ae5f1b43e2dcd9acf1d281c8f4155f
JhonSoto99 <jofeso-bamba@hotmail.com>
2024-11-25 11:16:26 -0500

    feat(models): Add foreing key model task.

--- tasks/models.py
+++ tasks/models.py
@@ -1,12 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Task(models.Model):
     """Model to store tasks."""
+
     title = models.CharField(max_length=128, help_text="Title of the task.")
     email = models.EmailField("Email associated with the task.")
     description = models.TextField(help_text="Description of the task.")
-    expiration_date = models.DateTimeField(help_text="Expiration date of the task.")
+    expiration_date = models.DateTimeField(
+        verbose_name="Expiration date", help_text="Expiration date of the task."
+    )
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="tasks"
+    )
 
     def __str__(self):
         return f"{self.title}"
